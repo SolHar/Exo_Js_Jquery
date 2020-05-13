1218310ce3505a59256974e5ca06f7fb422e9abd
solène haran <solene.haran@gmail.com>
2020-05-13 15:16:37 +0200

    dd

--- Script_JS.js
+++ Script_JS.js
@@ -33,10 +33,20 @@ function changeText() {
 //3. Sur le submit du formulaire:
 //o Ajouter un paragraphe dans un div externe au formulaire(on peut avoir
 //plusieurs paragraphes)
+
+function submitForm() {
+    var para = document.createElement('p');
+    para.innerText = 'test';
+    document.getElementById('container').appendChild(para);
 //o Le titre aura le contenu de la textbox
+
+    var title = document.createElement('h3');
+    
 //o Le contenu aura le contenu de la textblock
 //o Le paragraphe contiendra aussi un bouton permettant de supprimer ce
 //paragraphe
 //o Lors de l’ajout et de la suppression du paragraphe il doit y avoir une animation
 //o Le paragraphe aura un fond vert si la checkbox est cochée
 //o Lors de l’écriture dans la textbox, forcer l’écriture en majuscule
+
+}
