1d25350c11dec0107d953be27c99dc6d24377516
solène haran <solene.haran@gmail.com>
2020-05-13 13:20:17 +0200

    création des fichiers pour exo Full JS

---
+++ Script_JS.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+
+//1. Au chargement de la page:
+
+// o Ecrire « Hello World » dans la console
+
+
+/* o Modifier le contenu d’un span qui est dans votre page lors d’un click sur un
+bouton présent dans votre page*/
+//o Afficher les tables de multiplications dans une table généré en JS
+
+//2. Créez un formulaire HTML qui contient
+//o Une textbox
+//o Un textblock
+//o Une checkbox
+//o Un bouton submit
+
+
+//3. Sur le submit du formulaire:
+//o Ajouter un paragraphe dans un div externe au formulaire(on peut avoir
+//plusieurs paragraphes)
+//o Le titre aura le contenu de la textbox
+//o Le contenu aura le contenu de la textblock
+//o Le paragraphe contiendra aussi un bouton permettant de supprimer ce
+//paragraphe
+//o Lors de l’ajout et de la suppression du paragraphe il doit y avoir une animation
+//o Le paragraphe aura un fond vert si la checkbox est cochée
+//o Lors de l’écriture dans la textbox, forcer l’écriture en majuscule
