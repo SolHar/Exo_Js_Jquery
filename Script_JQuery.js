adf5bd9e524a30df559a485906a33722a8ac99ea
solène haran <solene.haran@gmail.com>
2020-05-16 14:10:28 +0200

    boot v2

--- Script_JQuery.js
+++ Script_JQuery.js
@@ -40,11 +40,28 @@
 //$(document).ready(function () { };
 //$(function () { })
 
-//--------------------------------------------------------------------------
+///if ($('#cbEstVert:checked').length)
+            contenu.css('backgroundColor', 'green');
+
+
+// Les points d'entrés
+//$(document).ready(function () {
+//    changeText();
+
+
+//function changeText() {
+//    $("#button").click(function () {
+//        $("#change").fadeOut(function () {
+//            $("#change").text(($("#change").text() == 'Un peu de texte') ? 'Good job' : 'Un peu de texte').fadeIn();
+//        })
+//    })
+//    }
+
+//});
+
 
 $(document).ready(function () {
     changeText();
-    uppercaseTitle()
 
 
     function changeText() {
@@ -59,64 +76,22 @@ $(document).ready(function () {
 
 //o Afficher les tables de multiplications dans une table généré en JS
 
-//var container = $('#multitable');
+var container = $('#multitable');
 
-//var newtable = $('<table>');
+var newtable = $('<table>');
 
-//for (var i = 0; i < 8; i++) {
-//    var ligne = $('<tr>');
+for (var i = 0; i < 8; i++) {
+    var ligne = $('<tr>');
 
-//    for (var j = 0; j < 8; i++) {
-//        var cellule = $('<td>');
-//        cellule.text(i * j);
-//        ligne.append(cellule);
+    for (var j = 0; j < 8; i++) {
+        var cellule = $('<td>');
+        cellule.text(i * j);
+        ligne.append(cellule);
 
-//    }
-//    container.append(table);
+    }
+    container.append(table);
 
-//}
-
-            // EXO 3 \\
-function uppercaseTitle() {
-
-    var chaine = $('#title').keyup(function () {
-    chaine.val(chaine.val().toUpperCase());
-    });
 }
-
-function submitForm(event) {
-    event.preventDefault();
-
-    
-        var container = $('#containerParagraphe');
-
-        var bloc = $('<div>');
-        var titre = $('<h1>');
-        var contenu = $('<p>');
-        var bouton = $('<input>');
-
-        titre.text($('#title').val());
-        contenu.text($('#content').val());
-
-        if ($('#check:checked').length) {
-            contenu.css('backgroundColor', 'green');
-        }
-
-        bouton.val('supprimer');
-        bouton.attr('type', 'submit');
-       
-       bouton.click(function () {
-            bloc.remove();
-       })
-        
-
-        bloc.append(titre);
-        bloc.append(contenu);
-        bloc.append(bouton);
-
-        container.append(bloc);
-   
-};
 
 
 
