a3f98aa89b79a793ab4d33f7015e3632f8a0ebbf
solène haran <solene.haran@gmail.com>
2020-05-17 03:00:45 +0200

    article en cours

--- Script_JS_article.js
+++ Script_JS_article.js
@@ -1,58 +1,57 @@
 // JavaScript source code
 
 window.addEventListener('load', function (e) {
+    //maj();
 
-    function maj() {
-        var chaine = document.getElementById('title').addEventListener('keyup', function () {
+    //function paragraphe() {
 
-            chaine.value = chaine.value.toUpperCase();
-        });
-    }
+    document.getElementById('title').addEventListener('keyup', function () {
+        this.value = this.value.toUpperCase();
+    //});
 
-    function submitForm() {
-        event.preventDefault();
+     document.getElementById('button').addEventListener('submit', function (e) {
 
-        document.getElementById('button').addEventListener('click', funtion (){
+            var createPara = document.createElement('p');
+            createPara.setAttribute('id', 'para');
 
-        
+            //o Le titre aura le contenu de la textbox
+
+            var createTitle = document.createElement('h3');
+            createTitle.innerText = document.getElementById('title').value;
+            createPara.appendChild(createTitle);
+
+            //o Le contenu aura le contenu de la textblock
+
+            var createContent = document.createElement('span');
+            createContent.innerText = document.getElementById('content').value;
+            createPara.appendChild(createContent);
+
+            //o Le paragraphe aura un fond vert si la checkbox est cochée
+
+            if (document.getElementsById('check').checked)
+                createPara.style.backgroundColor = 'green';
 
 
-        var createPara = document.createElement('p');
-        createPara.setAttribute('id', 'para');
+            //o Le paragraphe contiendra aussi un bouton permettant de supprimer ce
+            //paragraphe
 
-        //o Le titre aura le contenu de la textbox
+            var createButtonDel = document.createElement('input');
+            createButtonDel.setAttribute('type', 'submit');
+            createButtonDel.setAttribute('value', 'Supprimer');
+            createButtonDel.addEventListener('click', deletePara);
+            createPara.appendChild(createButtonDel);
 
-        var createTitle = document.createElement('h3');
-        createTitle.innerText = document.getElementById('title').value;
-        createPara.appendChild(createTitle);
 
-        //o Le contenu aura le contenu de la textblock
-
-        var createContent = document.createElement('span');
-        createContent.innerText = document.getElementById('content').value;
-        createPara.appendChild(createContent);
-
-        //o Le paragraphe aura un fond vert si la checkbox est cochée
-
-        if (document.getElementsById('check').checked) {
-            createPara.style.backgroundcolor('green');
-        }
-
-        //o Le paragraphe contiendra aussi un bouton permettant de supprimer ce
-        //paragraphe
-
-        var createButtonDel = document.createElement('input');
-        createButtonDel.setAttribute('type', 'submit');
-        createButtonDel.setAttribute('value', 'Supprimer');
-        createButtonDel.addEventListener('click', deletePara);
-        createPara.appendChild(createButtonDel);
-
-        function deletePara() {
             var delPara = document.getElementById('para');
             delPara.remove();
-        };
 
-        document.getElementById('container').appendChild(createPara);
+            document.getElementById('container').appendChild(createPara);
+
+        });
+
+        e.preventDefault();
+
     });
 });
+
 
--- Script_Jq_article.js
+++ Script_Jq_article.js
@@ -1,1 +1,36 @@
 // JavaScript source code
+
+$(document).ready(function () {
+
+    document.getElementById('title').addEventListener('keyup', function () {
+        this.value = this.value.toUpperCase();
+        $('#title').onkeyup(function () {
+            this.val() = this.val(toUpperCase);
+        })
+
+
+$('#container').submit(function () {
+    var containerPara = $('#container');
+
+    var bloc = $('<div>')
+    var titre = $('<h1>');
+    var contenu = $('<p>');
+    var bouton = $('<input>');
+
+    titre.text($('#title').val());
+    contenu.text($('#content').val());
+
+    if ($('#cbEstVert:checked').length)
+        contenu.css('backgroundColor', 'green');
+
+    bouton.val("Supprimer");
+    bouton.attr('type', 'submit');
+
+    bloc.append(titre);
+    bloc.ppend(content);
+    bloc.append(bouton);
+
+    containerPara.append(bloc);
+});
+
+};
